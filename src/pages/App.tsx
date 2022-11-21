0016a10e877fc8be05639d992e14d325293d0cb2
jdsteege <jdsteege@gmail.com>
2022-11-20 21:05:25 -0600

    Basic app routing

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,21 @@
-import Battle from "./BattlePOC";
+import { useAppState } from "~/states/AppState";
+import { GamePage } from "./GamePage";
 import MainMenu from "./MainMenu";
-
-
+import { Settings } from "./Settings";
 
 function App() {
-  let page = <MainMenu />;
+  const pageString: string = useAppState((state) => state.page);
 
-  return <>{page}</>;
+  let component: JSX.Element = <></>;
+  if (pageString == "main-menu") {
+    component = <MainMenu />;
+  } else if (pageString == "settings") {
+    component = <Settings />;
+  } else if (pageString == "gameInstance") {
+    component = <GamePage />;
+  }
+
+  return <>{component}</>;
 }
 
 export default App;
--- src/states/AppState.ts
+++ src/states/AppState.ts
@@ -2,13 +2,21 @@ import createStore from "zustand";
 import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
+export const stateActions = {
+  setPage: (value: string) => {
+    useAppState.setState((state: AppState) => {
+      state.page = value;
+    });
+  },
+};
+
 type AppState = { page: string };
 
 const appStateDefault: AppState = {
   page: "main-menu",
 };
 
-export const useInstance = createStore<AppState>()(
+export const useAppState = createStore<AppState>()(
   persist(
     immer<AppState>((set) => {
       return appStateDefault;
